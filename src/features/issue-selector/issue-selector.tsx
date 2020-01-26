5653c50bc16af9e9d001b6931802eedd0f5e8422
Martin Lang <martin@mercator.io>
2020-01-25 23:53:06 -0800

    add debouncing to search

---
+++ src/features/issue-selector/issue-selector.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface PublicProps {
+
+}
+type Props = PublicProps;
+
+export const IssueSelector: React.FC<Props> = props => {
+
+  return <div></div>
+}
--- src/features/repository-locator/components/repository-field/repository-field-component.tsx
+++ src/features/repository-locator/components/repository-field/repository-field-component.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { searchRepository_search_nodes_Repository } from '~gql/gql-schema';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useCombobox } from 'downshift';
 import { Input } from '~layout/input';
 import classNames from 'classnames';
+
+let debounceLock: NodeJS.Timeout | null;
 
 interface PublicProps {
   repositories: searchRepository_search_nodes_Repository[];
@@ -16,10 +18,22 @@ interface PublicProps {
 
 type Props = PublicProps;
 
-let debounceLock: number;
-
 export const RepositoryFieldComponent: React.FC<Props> = props => {
   const item = props.repositories.find(x => x.name === props.repositoryName);
+  const [searchTerm, setSearchTerm] = useState(props.repositoryName);
+
+  function debounceInput(input: string) {
+    setSearchTerm(input);
+    if (!debounceLock) {
+      debounceLock = setInterval(() => {
+        props.onNameChange(searchTerm);
+        if (debounceLock) {
+          clearTimeout(debounceLock);
+        }
+        debounceLock = null;
+      }, 300) as NodeJS.Timeout;
+    }
+  }
 
   const {
     isOpen,
@@ -34,7 +48,7 @@ export const RepositoryFieldComponent: React.FC<Props> = props => {
     items: props.repositories,
     itemToString: item => item.name,
     onInputValueChange: event => {
-      props.onNameChange(event.inputValue ?? '');
+      debounceInput(event.inputValue ?? '');
     },
     onSelectedItemChange: event => {
       if (event.selectedItem) {
